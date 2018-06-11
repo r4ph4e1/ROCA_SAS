f1935a2baabe50daa9cb8be1982839e6a4dfab4c
r4ph4e1 <raphael.luger@gmail.com>
2018-06-11 07:39:40 -0700

    Szenario Änderungen

--- szenario/decrypt.sh
+++ szenario/decrypt.sh
@@ -9,11 +9,16 @@ do
 	esac
 done
 
-
 #Entlüsseln des Key-Files "key.bin" mit dem Faktorisieren Private Key
 echo "Decrypting the Key-File with the factorized RSA-Private-Key"
 openssl rsautl -decrypt -inkey $PRIVKEY -in key.bin.enc -out key.bin
 
 # Entschlüsseln der AES verschlüsselten Datei mithilfe des entschlüsselten Key-Files:
-echo "Decrypting the File '' with the decrypted Key-File"
+echo "Decrypting the File $FILE with the decrypted Key-File"
 openssl enc -d -aes-256-cbc -in $FILE -out $OUT -pass file:./key.bin
+
+# Löschen der verschlüsselten Dokumente
+echo "Deleting Encrypted Files: $FILE and key.bin.enc"
+rm $FILE "key.bin.enc"
+
+
--- szenario/encrypt.sh
+++ szenario/encrypt.sh
@@ -1,8 +1,9 @@
-while getopts p:f: option
+while getopts p:f:o: option
 do
 	case "${option}" in
 		f) FILE=${OPTARG};;
 		p) PUBKEY=${OPTARG};;
+		o) OUT=${OPTARG};;
 	esac
 done
 
@@ -13,9 +14,12 @@ openssl rand -base64 35 -out key.bin
 
 # Verschlüsseln eines Files mit AES:
 echo "Encrypting the file $FILE with AES-256-CBC and the Key-File as Password"
-openssl enc -aes-256-cbc -salt -in $FILE -out $file.enc -pass file:./key.bin
+openssl enc -aes-256-cbc -salt -in $FILE -out  $OUT -pass file:./key.bin
 
 # Verschlüsseln des Key-Files "key.bin" mit unserem RSA Key
 echo "Encrypting the Key-File with the RSA-Public-Key: $PUBKEY"
 openssl rsautl -encrypt -inkey $PUBKEY -pubin -in key.bin -out key.bin.enc
 
+# Löschen des unverschlüsselten Dokuments
+echo "Deleting unencrypted Files: $FILE and key.bin"
+rm $FILE key.bin
